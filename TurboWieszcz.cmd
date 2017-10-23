df445549d71a342b0e76ba829c835a5e3884b39f
Jakub Noniewicz aka MoNsTeR of GDC / Noniewicz.com <32686195+monstergdc@users.noreply.github.com>
2017-10-23 12:11:00 +0200

    Windows file, has to have CRLFs, also added comment and "@"

--- TurboWieszcz.cmd
+++ TurboWieszcz.cmd
@@ -1,2 +1,7 @@
-TurboWieszcz.py 8 0 0
-pause
+rem Windows example, 
+rem parametres: number of verses, mode (0=ABAB, 1=ABBA, 2=AABB), repetitons ok (0=no, 1=yes)
+
+@TurboWieszcz.py 8 0 0
+
+
+@pause
